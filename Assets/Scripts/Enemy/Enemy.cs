7ec0babb562831db2be28379ff76105b2c33d11f
Yilmazyusuf42 <42yilmazyusuf@gmail.com>
2024-03-17 18:45:18 +0300

    EnemyAi

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,9 +6,8 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     GameObject mainCharachterPos;
-    Vector3 distanceBetween;
-    float speed = 1;
-    float rotationSpeed = 3f;
+    Vector3 moveDirection;
+    float speed = 5f;
 
     Rigidbody enemyRb;
     // Start is called before the first frame update
@@ -20,8 +19,31 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distanceBetween = (mainCharachterPos.transform.position - transform.position).normalized;
-        transform.Rotate(distanceBetween);
-        enemyRb.AddForce(distanceBetween * speed );
+        float distance = Vector3.Distance(mainCharachterPos.transform.position, transform.position);
+        Debug.Log(distance);
+        float attackDistance = 2f;
+        // Ýf distance bigger than attackDistance Movement keeps working
+        if (distance > attackDistance)
+        {
+            Movement();
+        }
+        else if (distance <= attackDistance)
+            Attack();
+
+
     }
+
+    void Movement()
+    {
+        moveDirection = (mainCharachterPos.transform.position - transform.position);
+        transform.LookAt(mainCharachterPos.transform);
+        enemyRb.velocity = moveDirection.normalized * speed ;
+    }
+
+
+    void Attack()
+    {
+        Debug.Log("attacking");
+    }
+    
 }
